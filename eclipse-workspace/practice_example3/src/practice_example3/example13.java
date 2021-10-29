ab857e26c2fc89fdaa96a5596ebe5427f1f02cdd
ryungeon <91059315+aypooo@users.noreply.github.com>
2021-10-29 16:43:04 +0900

    Add files via upload

--- eclipse-workspace/practice_example3/src/practice_example3/example13.java
+++ eclipse-workspace/practice_example3/src/practice_example3/example13.java
@@ -7,14 +7,16 @@ public class example13 {
 		
 		for(int i=0;i<100;i++) {
 			int a = i/10;
-			int b = i-i*a;
-			if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) { 
-				System.out.println(i+"¹Ú¼öÂ¦");
-					if (a == 3 || a == 6 || a == 9) 
-						System.out.println(i+"¹Ú¼öÂ¦");
-						if (a == 3 || a == 6 || a == 9) 
-							System.out.println(i+"¹Ú¼öÂ¦");
-			}	
+			int b = i-10*a;
+			if ((i % 10 == 3 || i % 10 == 6 || i % 10 == 9) || (a == 3 || a == 6 || a == 9)) {
+				System.out.print(i+"¹Ú¼öÂ¦");
+				
+			
+				if ((a == 3 || a == 6 || a == 9) && (b == 3 || b == 6 || b == 9))
+					System.out.print("Â¦");
+					System.out.println();
+				
+			}
 			
 		}
 	}
--- eclipse-workspace/practice_example3/src/practice_example3/example14.java
+++ eclipse-workspace/practice_example3/src/practice_example3/example14.java
@@ -1,9 +1,23 @@
 package practice_example3;
-
+import java.util.Scanner;
 public class example14 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		
+		String course[] = {"Java", "C++","HTML5","컴퓨터구조", "안드로이드"};
+		int score[] = {95, 88, 76, 62, 55};
+		
+		
+		for(int i=0;i<course.length;i++) {
+			if(course[i].equals(name)) {
+				System.out.println("과목이름>>"+course[i]);
+				System.out.println(course[i]+"의 점수는"+score[i]);
+			}
+			else
+				System.out.println("없는 과목입니다.");
+		}
 
 	}
 
